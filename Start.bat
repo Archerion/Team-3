b0672f4254d7820753df74e5bd6c112a58089673
jhklvdblre <frogra10@nith.no>
2012-09-03 12:28:12 +0200

    Removed unused projectile

--- Start.bat
+++ Start.bat
@@ -1,8 +1,10 @@
+@echo off
 C:\UDK\UDK-2012-07\Binaries\Win32\UDK.com make
 IF errorlevel 1 GOTO :ERROR_EXIT
-C:\UDK\UDK-2012-07\Binaries\Win32\UDK.exe MagicalTestMap.udk?game=MagicalFPSGame.MagicalGameInfo -log
+START C:\UDK\UDK-2012-07\Binaries\Win32\UDK.exe MagicalTestMap.udk?game=MagicalFPSGame.MagicalGameInfo -log
 GOTO :GOOD_EXIT
 :ERROR_EXIT
 pause
 exit /b %ERRORLEVEL%
 :GOOD_EXIT
+exit 0
